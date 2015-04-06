5b8b7106f76c0b35ba3537cef1efdcabe6a86701
Ashutosh <ashutosh@sendinblue.com>
2015-04-06 12:43:17 +0530

    small changes added

--- src/Medical/UserBundle/Controller/AdminController.php
+++ src/Medical/UserBundle/Controller/AdminController.php
@@ -13,9 +13,13 @@ class AdminController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $queryParams = [];
+        if ($this->getUser()) {
+            $queryParams['by_user'] = $this->getUser()->getId();
+        }
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-            $this->get('app.condition.repository')->getElements(['by_user' => $this->getUser()->getId()]),
+            $this->get('app.condition.repository')->getElements($queryParams),
             $request->query->get('page', 1),
             10
         );
